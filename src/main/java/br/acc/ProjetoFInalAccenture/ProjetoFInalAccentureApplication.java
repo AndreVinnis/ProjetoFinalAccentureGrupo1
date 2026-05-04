0c66e74000dd2f49c82dac77a8038dde38011589
AndreVinnis <andrev.macambira@gmail.com>
2026-05-04 15:43:42 -0300

    Initial commit

---
+++ src/main/java/br/acc/ProjetoFInalAccenture/ProjetoFInalAccentureApplication.java
@@ -0,0 +1,13 @@
+package br.acc.ProjetoFInalAccenture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFInalAccentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFInalAccentureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/acc/ProjetoFInalAccenture/ProjetoFInalAccentureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.acc.ProjetoFInalAccenture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFInalAccentureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
