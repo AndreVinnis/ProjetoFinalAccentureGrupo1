f4b2844cbcc2834d862fe146b971f35d8d305c92
horte <horte@LAPTOP-VIDA00F7>
2026-05-08 13:55:29 -0300

    fix it

--- backend/src/main/java/br/accenture/ProjetoFinalAccentureGrupo1/ProjetoFinalAccentureGrupo1Application.java
+++ backend/src/main/java/br/accenture/ProjetoFinalAccentureGrupo1/ProjetoFinalAccentureGrupo1Application.java
@@ -2,8 +2,14 @@ package br.accenture.ProjetoFinalAccentureGrupo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"br.accenture.ProjetoFinalAccentureGrupo1", "com.grupo.projeto.banking"})
+@EntityScan(basePackages = {"br.accenture.ProjetoFinalAccentureGrupo1", "com.grupo.projeto.banking"})
+@ComponentScan(basePackages = {"br.accenture.ProjetoFinalAccentureGrupo1", "com.grupo.projeto.banking"})
 public class ProjetoFinalAccentureGrupo1Application {
 
 	public static void main(String[] args) {
